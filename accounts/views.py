b05bfa9f85c52aeccb2bbb5ce2c8cb899f65b157
Phillip <phillip.pearson.1018@gmail.com>
2025-05-17 10:23:44 -0600

    registerUser form errors and alerts done

--- accounts/views.py
+++ accounts/views.py
@@ -1,32 +1,35 @@
-from django.http import HttpResponse
+from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import UserForm
 from .models import User
+from django.contrib import messages
 
 # Create your views here.
 def registerUser(request):
     if request.method == 'POST':
-        form = UserForm(request.POST)
+        form = UserForm(request.POST, request.FILES)
         if form.is_valid():
             # Create user using the form
-            password = form.cleaned_data['password']
-            user = form.save(commit=False)
-            user.set_password(password)
-            user.role = User.CUSTOMER
-            user.save()
-        else:
-            print('form error!!!!')
-            print(form.errors)
-            return redirect('registerUser')
-            #Create user using create_user method
-            # first_name = form.cleaned_data['first_name']
-            # last_name = form.cleaned_data['last_name']
-            # username = form.cleaned_data['username']
-            # email = form.cleaned_data['email']
             # password = form.cleaned_data['password']
-            # user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
-            # user.role= User.CUSTOMER
+            # user = form.save(commit=False)
+            # user.set_password(password)
+            # user.role = User.CUSTOMER
             # user.save()
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            user.role= User.CUSTOMER
+            user.save()
+            messages.success(request, 'Welcome to the Marketplace!')
+            return redirect('registerUser')
+        # else:
+        #     print('form error!!!!')
+        #     print(form.errors)
+        #     return redirect('registerUser')
+        #Create user using create_user method
     else:
         form = UserForm()
     context = {
